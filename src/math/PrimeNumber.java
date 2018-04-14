735ba4dcaf0c8589585b9e9e9e8e07b06c6c3b66
aneeqkhan007 <aneeqkhan2012@hotmail.com>
2018-04-14 09:09:25 -0400

    MidtemMarch2018

--- src/math/PrimeNumber.java
+++ src/math/PrimeNumber.java
@@ -10,30 +10,4 @@ package math;
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 *
 		 */
-		public class PrimeNumber {
-
-
-			public static void main(String[] args) {
-				long value;
-				long start;
-				long finish;
-
-				start = System.nanoTime();
-				value = arrayMethod(2000000);
-				finish = System.nanoTime();
-				System.out.printf("Value: %.3e, time: %4d ms\n", (double) value, (finish - start) / 1000000);
-
-				start = System.nanoTime();
-				value = treeMethod(2000000);
-				finish = System.nanoTime();
-				System.out.printf("Value: %.3e, time: %4d ms\n", (double) value, (finish - start) / 1000000);
-			}
-
-			private static long treeMethod(int i) {
-				return 0;
-			}
-
-			private static long arrayMethod(long i) {
-				return 0;
-			}
-		}
+		
