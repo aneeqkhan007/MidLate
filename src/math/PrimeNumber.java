e93249699378b1a2e27a48b7f8ac3a3f26b6e455
aneeqkhan007 <aneeqkhan2012@hotmail.com>
2018-04-14 09:39:52 -0400

    MidtemMarch2018

--- src/math/PrimeNumber.java
+++ src/math/PrimeNumber.java
@@ -1,6 +1,6 @@
 package math;
-
-
+public class PrimeNumber {
+	public static void main(String[] args) {
 		/*
 		 * Find list of Prime numbers from number 2 to 1 million.
 		 * Try the best solution as possible.Which will take less CPU life cycle.
@@ -10,4 +10,16 @@ package math;
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 *
 		 */
-		
+		int count = 1000000;
+		for (int x = 2; x <= count; x++) {
+			for (int y = 2; y <= x; y++) {
+				if (x == y) {
+					System.out.println(x);
+				}
+				if (x % y == 0) {
+					break;
+				}
+			}
+		}
+	}
+}
